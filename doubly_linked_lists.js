cd7baa3b8d066d68d4a1ebff96e6801131948c00
Jordan England-Nelson <JordanEnglandNelson@Jordans-MacBook-Air.local>
2017-04-04 07:20:04 -0700

    Creates doubly_linked_list file

---
+++ doubly_linked_lists.js
@@ -0,0 +1,50 @@
+// Node constructor
+
+class Node {
+  constructor(value){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+// Linked list contructor
+
+class LinkedList {
+  constructor(){
+    this.head = new Node("head");
+  }
+  _find(value){
+    let currNode = this.head;
+    while(currNode.value !== value){
+      currNode = this.next;
+    }
+    return currNode;
+  }
+  _findPrevious(value){
+    let currNode = this.head;
+    while(!(currNode.next == null) && (currNode.next !== value)){
+      currNode = currNode.next;
+    }
+    return currNode;
+  }
+  insert(value, location){
+    const newNode = new Node("value");
+    let currNode = this._find(location);
+    currNode.next = newNode.value;
+    newNode.next = currNode.next;
+  }
+  remove(value){
+    const nodeToDelete = this._find(value)
+    const prevNode = this._findPrevious(value);
+    while(nodeToDelete.next !== null){
+      prevNode.next = nodeToDelete.next;
+    }
+  }
+  display(){
+    let current = this.head;
+    while(!(current.next == null)){
+      console.log(current.next.value);
+      current = current.next;
+    }
+  }
+}
